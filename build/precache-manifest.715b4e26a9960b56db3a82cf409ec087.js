2badaab690d8320ece13386be134ec438831ba38
Neel <it.neel97@yahoo.com>
2020-09-30 21:57:23 -0400

    build added

---
+++ build/precache-manifest.715b4e26a9960b56db3a82cf409ec087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e205ef4303a01826147d49dbddf638d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2418a2093b54cb1e7043",
+    "url": "/static/css/main.52c2b07c.chunk.css"
+  },
+  {
+    "revision": "12ce7e90b029a840db52",
+    "url": "/static/js/2.c053824c.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/static/js/2.c053824c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2418a2093b54cb1e7043",
+    "url": "/static/js/main.dd40676a.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
